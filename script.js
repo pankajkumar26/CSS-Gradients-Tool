43f149168b57be6dce417d79784287f34e299d91
Pankaj Kumar <pankajpanwar3012@gmail.com>
2023-07-25 23:52:16 +0530

    version 1.0

---
+++ script.js
@@ -0,0 +1,65 @@
+const body = document.querySelector('body');
+const start = document.querySelector('#start');
+const stopButton = document.querySelector('#stop');
+const copyCSS = document.querySelector('#copyCSS');
+const copySuccess = document.querySelector('#copySuccess');
+const gradientColors = document.querySelector('.gradientColors');
+const colorInput1 = document.querySelector('#color1');
+const colorInput2 = document.querySelector('#color2');
+
+colorInput1.value = colorInput1.placeholder;
+colorInput2.value = colorInput2.placeholder;
+
+let intervalId;
+var colorCode;
+
+const startChangingBG = () => {
+    // start.style.opacity = 0.8;
+    colorCode = null;
+    const changeBG = () => {
+        let inputValue1 = colorInput1.value;
+        let inputValue2 = colorInput2.value;
+        colorCode = generateLinearGradientColors(inputValue1, inputValue2);
+        gradientColors.style.backgroundImage = colorCode;
+    }
+    start.disabled = true;
+    stopButton.disabled = false;
+    intervalId = setInterval(changeBG, 1500);
+}
+
+function generateLinearGradientColors(color1,color2){
+    return `linear-gradient(${generateRandomDegree()}deg, ${color1} 0%, ${color2} 65%)`;
+}
+
+function gennerateRandomPercent(){
+    return `${Math.ceil(Math.random()*100)}`
+}
+
+function generateRandomDegree(){
+    // Generate random number between -360 and +360 degrees inclusive of both ends
+    return Math.round((Math.random()*720)-360) ;
+}
+
+startChangingBG(); 
+
+start.addEventListener('click', startChangingBG)
+
+stopButton.addEventListener('click', function clear() {
+    start.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(intervalId);
+});
+
+
+copyCSS.addEventListener("click", (event) => {
+    backgroundImageCSS = `background : ${colorCode};`
+    navigator.clipboard.writeText(backgroundImageCSS);
+    copySuccess.style.visibility = 'visible';
+    setTimeout(()=>{
+        copySuccess.style.visibility = 'hidden';
+    },700)
+    event.preventDefault();
+});
+
+
+
